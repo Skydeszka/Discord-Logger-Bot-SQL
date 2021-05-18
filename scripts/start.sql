4d4b99611f38bceef1c29bdafe404d0c7850e35f
Skydeszka <skydeszka@gmail.com>
2021-05-18 19:56:18 +0200

    Remove log fetch table

--- scripts/start.sql
+++ scripts/start.sql
@@ -1,6 +1,7 @@
 CREATE TABLE messages(
     MessageID INTEGER PRIMARY KEY,
     Author TEXT,
+    AuthorID INTEGER,
     DateOfMessage DATE,
     Content TEXT
 );
@@ -8,15 +9,9 @@ CREATE TABLE messages(
 CREATE TABLE edits(
     MessageID INTEGER PRIMARY KEY,
     Author TEXT,
+    AuthorID INTEGER,
     DateOfOriginal DATE,
     DateOfEdit DATE,
     OriginalContent TEXT,
     EditedContent TEXT
 );
-
-CREATE TABLE logfetches(
-    ID INTEGER PRIMARY KEY AUTOINCREMENT,
-    FetchDate DATE,
-    Author TEXT,
-    FetchType VARCHAR(10)
-)
