c8b465035ff738e267d1b2baf7333369ac77a9ff
Skydeszka <norbiormandi@gmail.com>
2021-05-19 23:10:19 +0200

    Created setting table

--- scripts/start.sql
+++ scripts/start.sql
@@ -1,21 +1,29 @@
 CREATE TABLE messages(
-    MessageID INTEGER NOT NULL,
-    Author TEXT,
-    AuthorID INTEGER,
-    DateOfMessage DATE NOT NULL,
-    Content TEXT,
-    ChannelID INTEGER,
+    MessageID       INTEGER NOT NULL,
+    Author          TEXT,
+    AuthorID        INTEGER,
+    DateOfMessage   DATE NOT NULL,
+    Content         TEXT,
+    ChannelID       INTEGER,
     PRIMARY KEY (MessageID, DateOfMessage)
 );
 
 CREATE TABLE edits(
-    MessageID INTEGER NOT NULL,
-    Author TEXT,
-    AuthorID INTEGER,
-    DateOfOriginal DATE,
-    DateOfEdit DATE NOT NULL,
+    MessageID       INTEGER NOT NULL,
+    Author          TEXT,
+    AuthorID        INTEGER,
+    DateOfOriginal  DATE,
+    DateOfEdit      DATE NOT NULL,
     OriginalContent TEXT,
-    EditedContent TEXT,
-    ChannelID INTEGER,
+    EditedContent   TEXT,
+    ChannelID       INTEGER,
     PRIMARY KEY (MessageID, DateOfEdit)
 );
+
+CREATE TABLE setting (
+	PresetID    INTEGER NOT NULL,
+	LookbackMax	INTEGER NOT NULL,
+	PRIMARY KEY (PresetID AUTOINCREMENT)
+);
+
+INSERT INTO setting(LookbackMax) VALUES (100);
