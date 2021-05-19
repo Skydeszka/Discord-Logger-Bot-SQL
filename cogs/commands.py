b5d22029dfed6444e32b8f96c409c0f799120b2d
Skydeszka <norbiormandi@gmail.com>
2021-05-19 20:16:04 +0200

    Check edits, permission check

--- cogs/commands.py
+++ cogs/commands.py
@@ -18,6 +18,8 @@ class UserCommands(commands.Cog):
 
     # Prints last {amount} of messages from a specified user
     @commands.group(invoke_without_command=True, aliases=['message'])
+    @commands.guild_only()
+    @commands.has_permissions(manage_messages = True)
     async def messages(self, ctx, user: discord.Member, amount:int = 10):
         if amount <= lookback_maxamount:
             data = conn.execute(
@@ -31,6 +33,8 @@ class UserCommands(commands.Cog):
     
     # Prints last {amount} of messages between the two given dates
     @messages.command()
+    @commands.guild_only()
+    @commands.has_permissions(manage_messages = True)
     async def between(self, ctx, raw_date1: str, raw_date2: str, amount:int = 10):
         if amount <= lookback_maxamount:
             try:
@@ -50,6 +54,8 @@ class UserCommands(commands.Cog):
 
     # Prints last {amount} of messages between the given dates from a specified user
     @messages.command()
+    @commands.guild_only()
+    @commands.has_permissions(manage_messages = True)
     async def betweenfrom(self, ctx, raw_date1: str, raw_date2: str, user: discord.Member, amount:int = 10):
         if amount <= lookback_maxamount:
             try:
@@ -69,6 +75,8 @@ class UserCommands(commands.Cog):
 
     # Prints last {amount} of messages which contains the given keyword
     @messages.command()
+    @commands.guild_only()
+    @commands.has_permissions(manage_messages = True)
     async def contains(self, ctx, keyword: str, amount: int = 10):
         if amount <= lookback_maxamount:
             data = conn.execute(
@@ -83,6 +91,8 @@ class UserCommands(commands.Cog):
 
     # Prints last {amount} of messages which contains the given keyword sent by a specified user
     @messages.command()
+    @commands.guild_only()
+    @commands.has_permissions(manage_messages = True)
     async def containsfrom(self, ctx, keyword: str, user: discord.Member, amount: int = 10):
         if amount <= lookback_maxamount:
             data = conn.execute(
@@ -97,6 +107,8 @@ class UserCommands(commands.Cog):
 
     # Prints last {amount} of messages which contains the given keyword between the two given dates
     @messages.command()
+    @commands.guild_only()
+    @commands.has_permissions(manage_messages = True)
     async def containsbetween(self, ctx, keyword: str, raw_date1: str, raw_date2: str, amount: int = 10):
         if amount <= lookback_maxamount:
             try:
@@ -116,6 +128,8 @@ class UserCommands(commands.Cog):
 
     # Prints last {amount} of messages which contains the given keyword sent by a specified user between the two given dates
     @messages.command()
+    @commands.guild_only()
+    @commands.has_permissions(manage_messages = True)
     async def containsfrombetween(self, ctx, keyword: str, user: discord.Member, raw_date1: str, raw_date2: str, amount: int = 10):
         if amount <= lookback_maxamount:
             try:
@@ -146,6 +160,20 @@ class UserCommands(commands.Cog):
                 message += "Lookback limit decreased from {} to {}".format(pastLimit, lookback_maxamount)
             else:
                 message += "Attention: New limit equals to old limit."
+
+            await ctx.send(message)
+
+    # Prints last {amount} of edits from a specified user
+    @commands.group(invoke_without_command=True, aliases=["edit"])
+    @commands.guild_only()
+    @commands.has_permissions(manage_messages = True)
+    async def edits(self, ctx, user: discord.Member, amount:int = 10):
+        if amount <= lookback_maxamount:
+            data = conn.execute(
+                "SELECT * FROM edits WHERE AuthorID = ? LIMIT ?", (int(user.id), amount) 
+            )
+
+            message = log_to_edit(data)
 
             await ctx.send(message)
 
