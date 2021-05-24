a6c5c6a808de7481bf48e62f368d8e5e40efa302
Skydeszka <norbiormandi@gmail.com>
2021-05-24 21:33:29 +0200

    implement api

--- scripts/localhost.js
+++ scripts/localhost.js
@@ -2,10 +2,40 @@
 const express = require("express");
 const app = express();
 
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./database/logs.db3', sqlite3.OPEN_READONLY, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to database.');
+  });
+
+
+const port = process.env.PORT || 3000;
+
+
 app.get('/', (req, res) =>{
-    res.send("Hello world!");
+    res.send("How are you world?");
 });
 
-app.listen(3000, () =>{
-    console.log("Listen on port 3000");
+app.get('/api/logs', (req, res) =>{
+    var selector = "SELECT * FROM messages";
+
+    db.all(selector, [], (err, rows) =>{
+        if(err) throw err;
+        res.send(JSON.stringify(rows));
+    });
+});
+
+app.get('/api/logs/:msgid', (req, res) =>{
+    var selector = "SELECT * FROM messages WHERE MessageID = ?";
+
+    db.all(selector, [req.params.msgid], (err, rows) =>{
+        if(err) throw err;
+        res.send(JSON.stringify(rows));
+    });
+});
+
+app.listen(port, () =>{
+    console.log(`Listening on port ${port}...`);
 });
