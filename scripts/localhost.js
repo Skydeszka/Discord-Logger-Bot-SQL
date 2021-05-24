11b04b0b14fc8e3b247c9b3e328b4c121a720a19
Skydeszka <norbiormandi@gmail.com>
2021-05-24 23:41:38 +0200

    SelectBy Query Param

--- scripts/localhost.js
+++ scripts/localhost.js
@@ -19,12 +19,38 @@ app.get('/', (req, res) =>{
 });
 
 app.get('/api/logs', (req, res) =>{
-    const sortBy = req.query.sortBy;
+
+    const orderBy = req.query.orderBy;
+    let selectBy = req.query.selectBy;
 
     db.all("SELECT * FROM messages", [], (err, rows) => {
         if(err) throw err;
 
-        res.send(JSON.stringify(rows.find()))
+        let rawdata = {};
+
+        if(selectBy){
+            selectBy = selectBy.split(',');
+            if(selectBy[0].toLowerCase() == "name")
+                rawdata = rows.filter(x => x.Author.slice(0, -5) === selectBy[1]);
+            else if(selectBy[0].toLowerCase() == "id")
+                rawdata = rows.filter(x => x.AuthorID === selectBy[1]);
+            else if(selectBy[0].toLowerCase() == "since")
+                rawdata = rows.filter(x => new Date(x.DateOfMessage) >= new Date(selectBy[1]));
+            else if(selectBy[0].toLowerCase() == "between")
+                rawdata = rows.filter(x => {return new Date(x.DateOfMessage) >= new Date(selectBy[1]) && new Date(x.DateOfMessage) <= new Date(selectBy[2])});
+            else if(selectBy[0].toLowerCase() == "before")
+                rawdata = rows.filter(x => new Date(x.DateOfMessage) <= new Date(selectBy[1]));
+            else if(selectBy[0].toLowerCase() == "contains")
+                rawdata = rows.filter(x => x.Content.includes(selectBy[1]));
+        }
+        else
+            rawdata = rows;
+
+
+        if(rawdata.length == 0)
+            res.status(404).send("No match found");
+        else
+            res.send(JSON.stringify(rawdata));
     });
 });
 
