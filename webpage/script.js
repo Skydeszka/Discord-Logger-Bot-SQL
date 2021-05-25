4f2001b992b190db5be636800e79f23c068d8e7b
Skydeszka <norbiormandi@gmail.com>
2021-05-25 13:53:58 +0200

    get input

--- webpage/script.js
+++ webpage/script.js
@@ -1,5 +1,6 @@
 function log(){
     const author = document.getElementById("authorName").value;
+    const useID = document.getElementById("useID").value;
     const date1 = document.getElementById("date1").value;
     const date2 = document.getElementById("date2").value;
     const contains = document.getElementById("contains").value;
