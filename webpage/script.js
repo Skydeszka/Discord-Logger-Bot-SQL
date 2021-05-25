2c2c7c3ee00b6a4366b5caa21f2fc2cdbd057f2c
Skydeszka <norbiormandi@gmail.com>
2021-05-25 11:35:39 +0200

    Seperated from html

---
+++ webpage/script.js
@@ -0,0 +1,7 @@
+function log(){
+    const author = document.getElementById("authorName").value;
+    const date1 = document.getElementById("date1").value;
+    const date2 = document.getElementById("date2").value;
+    const contains = document.getElementById("contains").value;
+
+}
