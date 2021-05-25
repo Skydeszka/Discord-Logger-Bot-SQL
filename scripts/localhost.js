3325a25ae58fcb57527b2e1a702c7ff1e202532c
Skydeszka <norbiormandi@gmail.com>
2021-05-25 11:36:24 +0200

    Update localhost.js

--- scripts/localhost.js
+++ scripts/localhost.js
@@ -1,8 +1,17 @@
 // Module import
+const bodyParser = require("body-parser");
 const express = require("express");
-const app = express();
+const path = require("path")
+const fetch = require("node-fetch")
 
+// Setting up
+const app = express();
+app.set('view engine', 'pug');
+const stat_path = path.join(__dirname + "../../");
+app.use(express.static(stat_path));
+app.use(bodyParser.json());
 const sqlite3 = require('sqlite3').verbose();
+
 
 const port = process.env.PORT || 3000;
 
@@ -18,7 +27,16 @@ function open_db(){
 
 
 app.get('/', (req, res) =>{
-    res.send("How are you world?");
+
+    fetch(`http://localhost:${port}/api/logs`).then(res => {
+        return res.json();
+    })
+    .then(json => {
+        console.log(json)
+        res.render('index', {
+            messages: json
+        })
+    });
 });
 
 app.get('/api/logs', (req, res) =>{
@@ -54,7 +72,7 @@ app.get('/api/logs', (req, res) =>{
         if(rawdata.length == 0)
             res.status(404).send("No match found");
         else
-            res.send(JSON.stringify(rawdata));
+            res.json(rawdata);
     });
 
     db.close();
