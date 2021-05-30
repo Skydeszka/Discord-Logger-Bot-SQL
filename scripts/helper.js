a51371a489ecc772f51882a4b1a4bc0afcbec1bc
Skydeszka <norbiormandi@gmail.com>
2021-05-30 21:09:54 +0200

    Update helper.js

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,16 +1,24 @@
 function ParseBool(value){
-    if(isNaN(value)){
+    if(typeof value == "string"){
         if(value.toLowerCase() == "true")
             return true;
         else
             return false;
     }
-    else{
+    else if(typeof value == "number"){
         if(value == 1)
             return true
         else
             return false;
     }
+    else if(typeof value == "boolean"){
+        if(value)
+            return true;
+        else
+            return false;
+    }
+    else
+        return false;
 }
 
 module.exports = { ParseBool }
