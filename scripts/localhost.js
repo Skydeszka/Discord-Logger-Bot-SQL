eba6fa0e273ec1b0407f92ff1bc0e7284853fc4a
Skydeszka <norbiormandi@gmail.com>
2021-05-30 21:09:48 +0200

    changed static file location

--- scripts/localhost.js
+++ scripts/localhost.js
@@ -3,11 +3,13 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const path = require("path")
 const db = require("./database");
+const helper = require("./helper");
 
 // Setting up
 const app = express();
 app.set('view engine', 'pug');
-const stat_path = path.join(__dirname + "../../");
+const stat_path = path.join(__dirname + "../..//public");
+console.log(stat_path);
 app.use(express.static(stat_path));
 app.use(bodyParser.json());
 
@@ -15,12 +17,18 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 
 
+app.get('/', (req, res) =>{
+    res.send("Hello world");
+});
+
+
 app.get('/logs', (req, res) =>{
     const author = req.query.author;
-    const useID = req.query.useid;
+    const useID = helper.ParseBool(req.query.useid);
     const since = req.query.since;
     const before = req.query.before;
     const contains = req.query.contains;
+
 
 
     db.GetMessages(author, useID, since, before, contains).then(rows => {
