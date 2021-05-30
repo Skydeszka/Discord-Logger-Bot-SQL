57c910264dd4a96d470f3749628b411e559e7885
Skydeszka <norbiormandi@gmail.com>
2021-05-30 22:55:14 +0200

    added more params

--- public/edits/script.js
+++ public/edits/script.js
@@ -1,17 +1,23 @@
-function log(){
+function search(){
     const author = document.getElementById("authorName").value;
     const useID = document.getElementById("useID").checked;
-    const date1 = document.getElementById("date1").value;
-    const date2 = document.getElementById("date2").value;
-    const contains = document.getElementById("contains").value;
+    const origindate1 = document.getElementById("origindate1").value;
+    const origindate2 = document.getElementById("origindate2").value;
+    const editdate1 = document.getElementById("editdate1").value;
+    const editdate2 = document.getElementById("editdate2").value;
+    const origincontains = document.getElementById("origincontains").value;
+    const editcontains = document.getElementById("editcontains").value;
 
     let params = new URLSearchParams();
 
     if(author.length != 0) params.append("author", author);
     if(useID.length != 0) params.append("useid", useID);
-    if(date1.length != 0) params.append("since", date1);
-    if(date2.length != 0) params.append("before", date2);
-    if(contains.length != 0) params.append("contains", contains);
+    if(origindate1.length != 0) params.append("originsince", origindate1);
+    if(origindate2.length != 0) params.append("originbefore", origindate2);
+    if(editdate1.length != 0) params.append("editsince", editdate1);
+    if(editdate2.length != 0) params.append("editbefore", editdate2);
+    if(origincontains.length != 0) params.append("origincontains", origincontains);
+    if(editcontains.length != 0) params.append("editcontains", editcontains);
 
     window.location.href = window.location.origin + "/edits" + "?" + params;
 }
@@ -20,11 +26,12 @@ function log(){
 function reset(){
     const author = document.getElementById("authorName").value = null;
     const useID = document.getElementById("useID").checked = false;
-    const date1 = document.getElementById("date1").value = null;
-    const date2 = document.getElementById("date2").value = null;
-    const contains = document.getElementById("contains").value = null;
-}
+    const origindate1 = document.getElementById("origindate1").value = null;
+    const origindate2 = document.getElementById("origindate2").value = null;
+    const editdate1 = document.getElementById("editdate1").value = null;
+    const editdate2 = document.getElementById("editdate2").value = null;
+    const origincontains = document.getElementById("origincontains").value = null;
+    const editcontains = document.getElementById("editcontains").value = null;
 
-function test(){
-    alert("test");
+    window.location.href = window.location.origin + "/edits"
 }
